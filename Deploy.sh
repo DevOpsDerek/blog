45cdf25c6253f169de0dd4a4bcdbf831ef56522a
Derek Campbell <Derek.Campbell@Octopus.com>
2020-06-08 00:52:32 +0100

    Fixing Deployment Script

--- Deploy.sh
+++ Deploy.sh
@@ -1,1 +1,5 @@
 hugo --destination /Users/derekcampbell/Documents/GitHub/devopsderek.github.io
+cd /Users/derekcampbell/Documents/GitHub/devopsderek.github.io
+git add --all
+git commit -m "updating website"
+git push -f origin master
---
+++ content/post/blog4.md
@@ -0,0 +1,6 @@
+---
+title: "Blog4"
+date: 2020-06-08T00:47:45+01:00
+draft: false
+---
+
