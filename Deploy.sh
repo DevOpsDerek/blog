85fca522b5f9490a21c206b343ac001e13d94d8c
Derek Campbell <Derek.Campbell@Octopus.com>
2020-06-08 01:12:59 +0100

    Updating deployment script

--- Deploy.sh
+++ Deploy.sh
@@ -1,5 +1,11 @@
-hugo --destination /Users/derekcampbell/Documents/GitHub/devopsderek.github.io
-cd /Users/derekcampbell/Documents/GitHub/devopsderek.github.io
+#!/bin/bash
+# A script to publish the Hugo site
+DIR="/Users/derekcampbell/Documents/GitHub/devopsderek.github.io"
+d=$(date +%Y-%m-%d)
+
+hugo --destination "$DIR"
+cd "$DIR"
+echo "devopsderek.net" > CNAME
 git add --all
-git commit -m "updating website"
+git commit -m "updating website on $d"
 git push -f origin master
---
+++ content/post/blog6.md
@@ -0,0 +1,16 @@
+---
+title: "Blog6"
+date: 2020-06-08T01:08:09+01:00
+draft: false
+toc: false
+comments: false
+categories:
+- category1
+- category2
+tags:
+- tag1
+- tag2
+---
+
+
+<!--more-->
